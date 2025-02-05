6215cd764fe8857545f6efad8749dca5d71b6cc9
Sameer Kakade <samkakade44@gmail.com>
2025-02-05 13:48:01 +0530

    logging feature add with prometheus library

--- backend/routes/shortUrl.js
+++ backend/routes/shortUrl.js
@@ -2,10 +2,30 @@ import { Router } from "express";
 import ShortUrl from "../models/ShortUrl.js";
 import { nanoid } from "nanoid";
 import dotenv from "dotenv";
+import client from "prom-client";
+import express from "express";
 
 dotenv.config();
 
 const router = Router();
+
+//create a counter metric
+const counter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "status_code"],
+});
+
+export function requestCounter(req, res, next) {
+  res.on("finish", () => {
+    counter.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+  });
+  next();
+}
 
 // Create a short URL (anonymous)
 router.post("/shorten", async (req, res) => {
@@ -32,6 +52,20 @@ router.get("/:shortCode", async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Server error" });
   }
+});
+
+router.get("/v1/test", async (req, res) => {
+  try {
+    res.send("Hello sameer");
+  } catch (error) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+router.get("/v1/metrics", async (req, res) => {
+  const metrics = await client.register.metrics();
+  res.set("Content-Type", client.register.contentType);
+  res.end(metrics);
 });
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import ShortUrl from "./models/ShortUrl.js";
 import dotenv from "dotenv";
 import express from "express";
 import router from "./routes/shortUrl.js";
+import { requestCounter } from "./routes/shortUrl.js";
 
 import cors from "cors";
 
@@ -12,13 +13,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(requestCounter);
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI, {});
 
-app.use("/", router);
+app.use("/api", router);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
